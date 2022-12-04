ab6ffd2a172a2eec1fc31db5c87211f4ff7ce9da
cgrubio91 <104238457+cgrubio91@users.noreply.github.com>
2022-12-04 14:25:09 -0500

    Actualización App.js
    Se agrega la función y solución de la HU5 - Buscar película

--- my-movies/src/App.js
+++ my-movies/src/App.js
@@ -1,15 +1,45 @@
-import './App.css';
-import CardMovie from './component/CardMovie';
-import MoviesJson from './movies.json'
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import "./App.css";
+import CardMovie from "./component/CardMovie";
+import MoviesJson from "./movies.json";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
   return (
     <div className="App">
+      {" "}
+      <input type="text" name="buscador" id="buscador" placeholder="Buscar..."></input>
+      <ul id="listaPeliculas">
+        {MoviesJson.map(movie =><li className="title" > {movie.original_title}</li>)}
+      </ul>
+      {
+      document.addEventListener("keyup", e=>{
+
+        if(e.target.matches("#buscador")){
+
+            if(e.key ==="Escape")e.target.value = ""
+
+            document.querySelectorAll(".title").forEach(pelicula =>{
+
+                pelicula.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+                  ?pelicula.classList.remove("filtro")
+                  :pelicula.classList.add("filtro")
+            })
+
+        }
+
+})}
       <Container>
-        <h1 style={{ color: "white" }}> Peliculas Favoritas </h1>
+        {" "}
+        <h1
+          style={{
+            color: "white",
+          }}
+        >
+          {" "}
+          Peliculas Favoritas{" "}
+        </h1>{" "}
         <Row>
           {" "}
           {MoviesJson.map((movie) => {
@@ -19,18 +49,19 @@ function App() {
                   {" "}
                   <CardMovie
                     image={
-                      "https://image.tmdb.org/t/p/w300" + movie.poster_path
+                      "https://image.tmdb.org/t / p / w300 " + movie.poster_path
                     }
                     title={movie.title}
-                  />
+                  />{" "}
                 </Col>
               );
             }
-          })}
-        </Row>
-      </Container>
+          })}{" "}
+        </Row>{" "}
+      </Container>{" "}
       <Container>
-        <h1 style={{ color: "white" }}> Peliculas </h1>
+        {" "}
+        <h1 style={{ color: "white" }}> Peliculas </h1>{" "}
         <Row>
           {" "}
           {MoviesJson.map((movie) => {
@@ -40,10 +71,10 @@ function App() {
                   {" "}
                   <CardMovie
                     image={
-                      "https://image.tmdb.org/t/p/w300" + movie.poster_path
+                      "https://image.tmdb.org/t / p / w300 " + movie.poster_path
                     }
                     title={movie.title}
-                  />
+                  />{" "}
                 </Col>
               );
             }
@@ -51,6 +82,7 @@ function App() {
         </Row>{" "}
       </Container>{" "}
       <Container>
+        {" "}
         <h1 style={{ color: "white" }}> Peliculas para Adultos </h1>{" "}
         <Row>
           {" "}
@@ -61,18 +93,22 @@ function App() {
                   {" "}
                   <CardMovie
                     image={
-                      "https://image.tmdb.org/t/p/w300" + movie.poster_path
+                      "https://image.tmdb.org/t / p / w300 " + movie.poster_path
                     }
                     title={movie.title}
-                  />
+                  />{" "}
                 </Col>
               );
             }
           })}{" "}
         </Row>{" "}
-      </Container>
+      </Container>{" "}
       <Container>
-        <h1 style={{ color: "white" }}> Peliculas para Toda la familia </h1>{" "}
+        {" "}
+        <h1 style={{ color: "white" }}>
+          {" "}
+          Peliculas para Toda la familia{" "}
+        </h1>{" "}
         <Row>
           {" "}
           {MoviesJson.map((movie) => {
@@ -82,18 +118,17 @@ function App() {
                   {" "}
                   <CardMovie
                     image={
-                      "https://image.tmdb.org/t/p/w300" + movie.poster_path
+                      "https://image.tmdb.org/t / p / w300 " + movie.poster_path
                     }
                     title={movie.title}
-                  />
+                  />{" "}
                 </Col>
               );
             }
           })}{" "}
         </Row>{" "}
-      </Container>
+      </Container>{" "}
     </div>
   );
 }
-
 export default App;
