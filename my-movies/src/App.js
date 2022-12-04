c424d1a9c08e3656241a89b9acecf7391cd322a5
unknown <wandrade@ibero.edu.co>
2022-12-04 10:17:14 -0500

    Ajuste formato documento

--- my-movies/src/App.js
+++ my-movies/src/App.js
@@ -6,86 +6,94 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function App() {
-    return ( < div className = "App" >
-        <
-        Container >
-        <
-        h1 style = {
-            { color: "white" }
-        } > Peliculas Favoritas < /h1> <
-        Row > {
-            MoviesJson.map(
-                movie => {
-                    if (movie.favorite === true) {
-                        return <Col > < CardMovie image = { "https://image.tmdb.org/t/p/w300" + movie.poster_path }
-                        title = { movie.title }
-                        /></Col >
+  return (
+    <div className="App">
+      <Container>
+        <h1 style={{ color: "white" }}> Peliculas Favoritas </h1>
+        <Row>
+          {" "}
+          {MoviesJson.map((movie) => {
+            if (movie.favorite === true) {
+              return (
+                <Col>
+                  {" "}
+                  <CardMovie
+                    image={
+                      "https://image.tmdb.org/t/p/w300" + movie.poster_path
                     }
-                }
-            )
-        } <
-        /Row> < /
-        Container > <
-        Container >
-        <
-        h1 style = {
-            { color: "white" }
-        } > Peliculas < /h1> <
-        Row > {
-            MoviesJson.map(
-                movie => {
-                    if (movie.favorite === false) {
-                        return <Col > < CardMovie image = { "https://image.tmdb.org/t/p/w300" + movie.poster_path }
-                        title = { movie.title }
-                        /></Col >
+                    title={movie.title}
+                  />
+                </Col>
+              );
+            }
+          })}
+        </Row>
+      </Container>
+      <Container>
+        <h1 style={{ color: "white" }}> Peliculas </h1>
+        <Row>
+          {" "}
+          {MoviesJson.map((movie) => {
+            if (movie.favorite === false) {
+              return (
+                <Col>
+                  {" "}
+                  <CardMovie
+                    image={
+                      "https://image.tmdb.org/t/p/w300" + movie.poster_path
                     }
-                }
-            )
-        } <
-        /Row> < /
-        Container > <
-        Container >
-        <
-        h1 style = {
-            { color: "white" }
-        } > Peliculas para Adultos < /h1> <
-        Row > {
-            MoviesJson.map(
-                movie => {
-                    if (movie.adult === true) {
-                        return <Col > < CardMovie image = { "https://image.tmdb.org/t/p/w300" + movie.poster_path }
-                        title = { movie.title }
-                        /></Col >
+                    title={movie.title}
+                  />
+                </Col>
+              );
+            }
+          })}{" "}
+        </Row>{" "}
+      </Container>{" "}
+      <Container>
+        <h1 style={{ color: "white" }}> Peliculas para Adultos </h1>{" "}
+        <Row>
+          {" "}
+          {MoviesJson.map((movie) => {
+            if (movie.adult === true) {
+              return (
+                <Col>
+                  {" "}
+                  <CardMovie
+                    image={
+                      "https://image.tmdb.org/t/p/w300" + movie.poster_path
                     }
-                }
-            )
-        } <
-        /Row> < /
-        Container >
-        <
-        Container >
-        <
-        h1 style = {
-            { color: "white" }
-        } > Peliculas para Toda la familia < /h1> <
-        Row > {
-            MoviesJson.map(
-                movie => {
-                    if (movie.adult === false) {
-                        return <Col > < CardMovie image = { "https://image.tmdb.org/t/p/w300" + movie.poster_path }
-                        title = { movie.title }
-                        /></Col >
+                    title={movie.title}
+                  />
+                </Col>
+              );
+            }
+          })}{" "}
+        </Row>{" "}
+      </Container>
+      <Container>
+        <h1 style={{ color: "white" }}> Peliculas para Toda la familia </h1>{" "}
+        <Row>
+          {" "}
+          {MoviesJson.map((movie) => {
+            if (movie.adult === false) {
+              return (
+                <Col>
+                  {" "}
+                  <CardMovie
+                    image={
+                      "https://image.tmdb.org/t/p/w300" + movie.poster_path
                     }
-                }
-            )
-        } <
-        /Row> < /
-        Container >
-
-
-        <
-        /div>
-    );
+                    title={movie.title}
+                  />
+                </Col>
+              );
+            }
+          })}{" "}
+        </Row>{" "}
+      </Container>
+    </div>
+  );
 }
 
 export default App;
